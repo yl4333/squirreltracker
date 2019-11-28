22fb16d12358c956b93fddee2f57dc7d7e1116ba
Yunqi Liu <yunqiliu@Yunqis-MacBook-Pro.local>
2019-11-28 12:39:01 -0500

    import

---
+++ sightings/management/commands/import_squirrel_data.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand, CommandError
+from sightings.models import Squirrel
+import os
+import csv
+from datetime import datetime
+
+class Command(BaseCommand):
+    help = 'Use csv file to import squirrel data'
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', nargs='?', type=str)
+
+    def handle(self, *args, **options):
+        with open(options['file_path'], 'r') as csvfile:
+            csvreader = csv.DictReader(csvfile, delimiter=',')
+            # print(csvreader.fieldnames)
+            for row in csvreader:
+                squirrels, created = Squirrel.objects.get_or_create(
+                    squirrelid = row['Unique Squirrel ID'],
+                    longtitude = row['X'],
+                    latitude = row['Y'],
+                    shifts = row['Shift'],
+                    date = datetime.strptime(row['Date'], '%m%d%Y').date(),
+                    age = row['Age'],
+                    color = row['Primary Fur Color'],
+                    location = row['Location'],
+                    specific_location = row['Specific Location'],
+                    running = (row['Running'].lower() == 'true'),
+                    chasing = (row['Chasing'].lower() == 'true'),
+                    climbing = (row['Climbing'].lower() == 'true'),
+                    eating = (row['Eating'].lower() == 'true'),
+                    foraging = (row['Foraging'].lower() == 'true'),
+                    other_activities = row['Other Activities'],
+                    kuks = (row['Kuks'].lower() == 'true'),
+                    quaas = (row['Quaas'].lower() == 'true'),
+                    moans = (row['Moans'].lower() == 'true'),
+                    tail_flags = (row['Tail flags'].lower() == 'true'),
+                    tail_twitches = (row['Tail twitches'].lower() == 'true'),
+                    approaches = (row['Approaches'].lower() == 'true'),
+                    indifferent = (row['Indifferent'].lower() == 'true'),
+                )
+                if created:
+                    squirrels.save()
+            
--- sightings/models.py
+++ sightings/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-class squirrel(models.Model):
-    id = models.CharField(
-        max_length=200,
-        primary_key=True,
+class Squirrel(models.Model):
+    squirrelid = models.CharField(
+        max_length=255,
         help_text="Unique Squirrel ID",
     )
 
@@ -19,35 +18,146 @@ class squirrel(models.Model):
         help_text="latitude",
     )
 
-    AM = "am"
-    PM = "pm"
-    SHIFTS = [
-        (AM, "am"),
-        (PM, "pm"),
+    AM = "AM"
+    PM = "PM"
+    SHIFTS_CHOICES = [
+        (AM, "AM"),
+        (PM, "PM"),
     ]
 
     shifts = models.CharField(
-        max_length=200,
-        choices=SHIFTS,
+        max_length=255,
+        choices=SHIFTS_CHOICES,
         default=None,
-        help_text="shifts"
+        help_text="shifts",
     )
 
     date = models.DateField(
-        help_text="shifts"
+        help_text="shifts",
     )
 
     ADULT = "Adult"
     JUVENILE = "Juvenile"
 
-    AGE = [
+    AGE_CHOICES = [
         (ADULT, "Adult"),
         (JUVENILE, "Juvenile"),
     ]
 
     age = models.CharField(
-        max_length=200,
-        choices=AGE,
+        max_length=255,
+        choices=AGE_CHOICES,
         default=None,
-        help_text="age"
+        help_text="age",
     )
+
+    GRAY = "Gray"
+    CINNAMON = "Cinnamon"
+    BLACK = "Black"
+
+    COLOR_CHOICES = [
+        (GRAY, "Gray"),
+        (CINNAMON, "Cinnamon"),
+        (BLACK, "Black"),
+    ]
+
+    color = models.CharField(
+        max_length=255,
+        choices=COLOR_CHOICES,
+        default=None,
+        help_text="Primary Fur Color",
+    )
+
+    GROUND_PLANE = "Ground Plane"
+    ABOVE_GROUND = "Above Ground"
+
+    LOCATION_CHOICES = [
+        (GROUND_PLANE, "Ground Plane"),
+        (ABOVE_GROUND, "Above Ground"),
+    ]
+
+    location = models.CharField(
+        max_length=255,
+        choices=LOCATION_CHOICES,
+        default=None,
+        help_text="Location",
+    )
+
+    specific_location = models.CharField(
+        max_length=255,
+        help_text="Specific Location",
+    )
+
+    running = models.BooleanField(
+        default=False,
+        help_text="Running",
+    )
+
+    chasing = models.BooleanField(
+        default=False,
+        help_text="Chasing",
+    )
+
+    climbing = models.BooleanField(
+        default=False,
+        help_text="Climbing",
+    )
+
+    eating = models.BooleanField(
+        default=False,
+        help_text="Eating",
+    )
+
+    foraging = models.BooleanField(
+        default=False,
+        help_text="Foraging",
+    )
+
+    other_activities = models.CharField(
+        max_length=255,
+        help_text="Other activities",
+    )
+
+    kuks = models.BooleanField(
+        default=False,
+        help_text="Kuks",
+    )
+
+    quaas = models.BooleanField(
+        default=False,
+        help_text="Quaas",
+    )
+
+    moans = models.BooleanField(
+        default=False,
+        help_text="Moans",
+    )
+
+    tail_flags = models.BooleanField(
+        default=False,
+        help_text="Tail flags",
+    )
+
+    tail_twitches = models.BooleanField(
+        default=False,
+        help_text="Tail twitches",
+    )
+
+    approaches = models.BooleanField(
+        default=False,
+        help_text="Approaches",
+    )
+
+    indifferent = models.BooleanField(
+        default=False,
+        help_text="indifferent",
+    )
+
+    runs_from = models.BooleanField(
+        default=False,
+        help_text="Runs from",
+    )
+
+    def __str__(self):
+        return "Squirrel" + self.squirrelid
+

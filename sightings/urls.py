7db2f3471adf808f23eb9b4c1f6282ff0edb1472
e <jamerryding@gmail.com>
2019-12-06 22:31:00 -0500

    update the views and templates of sightings

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('/add', views.add, name='add'),
-    # path('<str:squirrel_id>/', views.update, name='update'),
+    path('add/', views.add, name='add'),
+    path('<str:squirrelid>/', views.update, name='update'),
+    path('stats/', views.stats, name='stats'),
 ]

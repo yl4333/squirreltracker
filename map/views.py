0ae471281261f228c33e1ea0d23306301f58bb4b
Yunqi Liu <yunqiliu@Yunqis-MacBook-Pro.local>
2019-12-08 10:43:17 -0500

    export command

--- map/views.py
+++ map/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from sightings.models import Squirrel
+# pylint: disable=no-member
+
 
 def index(request):
     squirrels = Squirrel.objects.all()

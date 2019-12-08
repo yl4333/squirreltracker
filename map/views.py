4eb8010cca96b037c5233294c00fb9ab9e4bd055
Yunqi Liu <yunqiliu@Yunqis-MacBook-Pro.local>
2019-12-08 09:18:31 -0500

    map

--- map/views.py
+++ map/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from sightings.models import Squirrel
 
 def index(request):
-    return HttpResponse("Map test")
+    squirrels = Squirrel.objects.all()
+    context = {
+        'squirrels': squirrels[:99]
+    }
+    return render(request, 'map.html', context)
